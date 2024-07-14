266efe908cbc602ba14dbd844e944041f4a2aed9
Xinlei Feng <xinlei_feng@umail.ucsb.edu>
2024-07-13 22:05:02 -0700

    Complete Project 2 Challenge: store user's score; alert user when the answer is wrong; set only 8 questions for the game, allow restart the game.

--- SwiftUI/project2/GuessTheFlag/ContentView.swift
+++ SwiftUI/project2/GuessTheFlag/ContentView.swift
@@ -3,6 +3,7 @@
 //  GuessTheFlag
 //
 //  Created by Paul Hudson on 11/10/2023.
+//  Modified by Xinlei Feng on 07/13/2024.
 //
 
 import SwiftUI
@@ -13,7 +14,11 @@ struct ContentView: View {
 
     @State private var showingScore = false
     @State private var scoreTitle = ""
-
+    @State private var userScore = 0
+    @State private var chosenFlag = 0
+    @State private var questionLeft = 8
+    @State private var showingRestart = false
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
@@ -57,7 +62,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
 
-                Text("Score: ???")
+                Text("Score: \(userScore)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
 
@@ -68,23 +73,46 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            if scoreTitle == "Correct" {
+                Text("Your score is \(userScore)")
+            } else {
+                Text("Wrong! That's the flag of \(countries[chosenFlag])")
+            }
+        }
+        
+        .alert("You completed 8 questions", isPresented: $showingRestart) {
+            Button("Restart", action: reset)
+        } message: {
+            Text("Your final score is \(userScore). Restart the game?")
         }
     }
 
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         } else {
             scoreTitle = "Wrong"
+            userScore -= 1
         }
 
         showingScore = true
+        chosenFlag = number
+        questionLeft -= 1
+        if questionLeft == 0 {
+            showingRestart = true
+        }
     }
 
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        questionLeft = 8
+        userScore = 0
+        askQuestion()
     }
 }
 
