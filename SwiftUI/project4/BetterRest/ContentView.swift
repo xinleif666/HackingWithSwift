dd4bd0964a5c2562d6a9246987ba59aac52ba3cd
Xinlei Feng <xinlei_feng@umail.ucsb.edu>
2024-07-14 19:01:19 -0700

    Complete Project 4 Challenge: 1. Replace each VStack in our form with a Section. 2. Replace the “Number of cups” stepper with a Picker showing the same range of values. 3. Change the user interface so that it always shows their recommended bedtime using a nice and large font.

--- SwiftUI/project4/BetterRest/ContentView.swift
+++ SwiftUI/project4/BetterRest/ContentView.swift
@@ -3,6 +3,7 @@
 //  BetterRest
 //
 //  Created by Paul Hudson on 15/10/2023.
+//  Modified by Xinlei Feng on 07/14/2024.
 //
 
 import CoreML
@@ -12,10 +13,6 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
 
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -27,61 +24,52 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-
+                Section("When do you want to wake up?") {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                 }
 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-
+                Section("Desired amount of sleep") {
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-
-                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
+                Section("Daily coffee intake") {
+                    Picker("Number of cup", selection: $coffeeAmount) {
+                        ForEach(1..<20) {
+                            Text("\($0) cups")
+                        }
+                    }
                 }
+                
+                Section("Your ideal bed time is") {
+                    Text(self.calculateBedtime())
+                        .font(.largeTitle)
+                        .foregroundStyle(.indigo)
+                }
+                .font(.callout)
+                .foregroundStyle(.cyan)
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") { }
-            } message: {
-                Text(alertMessage)
-            }
         }
     }
 
-    func calculateBedtime() {
+    func calculateBedtime() -> String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
-
+            
             let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
-
+            
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-
+            
             let sleepTime = wakeUp - prediction.actualSleep
-
-            alertTitle = "Your ideal bedtime is…"
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+            return sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "Sorry, there was a problem calculating your bedtime."
         }
-
-        showingAlert = true
     }
 }
 
